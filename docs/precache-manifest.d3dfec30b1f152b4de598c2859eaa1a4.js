e73c6ecb073cb9413b68416d999dc13d9a8b7671
sportsman701 <sportsman701@outlook.com>
2019-12-01 17:25:26 +0700

    docs added

---
+++ docs/precache-manifest.d3dfec30b1f152b4de598c2859eaa1a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e47da2af574e2533821e26eab3cd6a0d",
+    "url": "/Pokedx/index.html"
+  },
+  {
+    "revision": "557913763e87044bb556",
+    "url": "/Pokedx/static/css/2.90172c5a.chunk.css"
+  },
+  {
+    "revision": "8927118c16251776e103",
+    "url": "/Pokedx/static/css/main.1f612b0e.chunk.css"
+  },
+  {
+    "revision": "557913763e87044bb556",
+    "url": "/Pokedx/static/js/2.0d4af34a.chunk.js"
+  },
+  {
+    "revision": "8927118c16251776e103",
+    "url": "/Pokedx/static/js/main.a8aff60a.chunk.js"
+  },
+  {
+    "revision": "5f5a9937b0f1c8f18b24",
+    "url": "/Pokedx/static/js/runtime-main.89a59c3f.js"
+  }
+]);
